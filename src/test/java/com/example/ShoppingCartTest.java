04784166289c34837fd7a2014d83461b571b4573
Arda Halim <arda.halim@iths.se>
2025-01-28 14:15:49 +0100

    add and remove products

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,6 +1,34 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ShoppingCartTest {
 
+    private ShoppingCart shoppingCart;
 
+    @BeforeEach
+    void setUp() {
+        shoppingCart = new ShoppingCart();
+    }
+
+    @Test
+    void shouldAddProductToCart() {
+        Product product = new Product("Product 1", 100.0);
+
+        shoppingCart.addProduct(product, 2);
+
+        asssertThat(shoppingCart.getTotalPrice()).isEqualTo(200.0);
+    }
+
+    @Test
+    void shouldRemoveProductFromCart() {
+        Product product = new Product("Product 1", 100.0);
+
+        shoppingCart.addProduct(product, 2);
+
+        shoppingCart.removeProduct(product, 1);
+
+        assertThat(shoppingCart.getTotalPrice()).isEqualTo(100.0);
+    }
 }
